d2804503abf7cd288400e70c38fa91befa17429c
Andrew Barba <abarba.77@gmail.com>
2014-11-19 23:14:26 -0500

    logging

--- http/lib/cache.js
+++ http/lib/cache.js
@@ -7,18 +7,29 @@ var stream = require("stream");
 var util = require("util");
 
 /*============================================================================*
+ * Constants                                                                  *
+ *============================================================================*/
+
+var MAX_BYTES = 10 * 1000 * 1000;
+
+/*============================================================================*
  * Stream                                                                     *
  *============================================================================*/
 
-var TransformStream = function(key, cache) {
+var TransformStream = function(key, cache, next) {
 	this.readable = true;
 	this.writable = true;
 	this.key = key;
 	this.cache = cache;
 	this.data = "";
+	this.onComplete = next || function(){};
 };
 util.inherits(TransformStream, stream);
 
+/**
+ * Buffer incoming data into existing data
+ * Emit data event to next pipe
+ */
 TransformStream.prototype.buffer = function(data) {
  	if (data) {
  		this.data += data;
@@ -26,30 +37,84 @@ TransformStream.prototype.buffer = function(data) {
  	}
 };
 
+/**
+ * Write data through next pipe
+ */
 TransformStream.prototype.write = function () {
-  this.buffer.apply(this, arguments);
+ 	this.buffer.apply(this, arguments);
 };
 
+/**
+ * All data is in the buffer.
+ * Update cache size and evict something
+ * if necessary. Call completion handler
+ */
 TransformStream.prototype.end = function () {
-  this.buffer.apply(this, arguments);
-  this.cache[this.key] = this.data;
-  this.emit("end");
+	this.buffer.apply(this, arguments);
+
+	// get final data
+	var data = this.data;
+
+	// update cache and size
+	this.cache.size += (new Buffer(data)).length;
+	this.cache.cache[this.key] = data;
+
+	// check if we need to remove something
+	this.cache.evict();
+
+	// emit end event and call completion handler
+	this.emit("end");
+	this.onComplete();
 };
 
 /*============================================================================*
- * Cache                                                                     *
+ * Cache                                                                      *
  *============================================================================*/
 
 var Cache = function(){
 	this.cache = {};
+	this.size = 0;
 };
 
+/**
+ * Get data associated with given key
+ * null if it does not exist
+ */
 Cache.prototype.get = function(key) {
 	return this.cache[key];
 };
 
-Cache.prototype.set = function(key) {
-	return new TransformStream(key, this.cache);
+/**
+ * Cache incoming data via a stream
+ * Completion handler will be called
+ * when all data has been cached
+ */
+Cache.prototype.set = function(key, next) {
+	return new TransformStream(key, this, next);
+};
+
+/**
+ * Remove a single item from the cache
+ * and update cache size
+ */
+Cache.prototype.remove = function(key) {
+	var data = this.cache[key];
+	if (data) {
+		var size = this.size - (new Buffer(data)).length;
+		this.size = Math.max(0, size);
+		delete this.cache[key];
+	}
+};
+
+/**
+ * Evict an item from the cache if
+ * we exceeded our memory limit
+ */
+Cache.prototype.evict = function() {
+	if (this.size > MAX_BYTES) {
+		var keys = Object.keys(this.cache);
+		this.remove(keys[0]);
+	}
 };
 
 module.exports = new Cache();
--- http/lib/server.js
+++ http/lib/server.js
@@ -10,24 +10,40 @@ var cache = require('./cache');
 var PORT = process.argv[3];
 var ORIGIN = process.argv[5];
 
+// Response time
+var RESPONSE_TIME = 0;
+
 // Create server
 var server = http.createServer(function (req, res) {
  	
+ 	// start time
+ 	var start = Date.now();
+
 	// origin url
  	var url = util.format("http://%s%s", ORIGIN, req.url);
+ 	console.log('GET ' + url);
 
  	// check cache
  	var data = cache.get(url);
 
- 	// if we have data, return it
- 	// else fetch from origin and cache it
+ 	// if we have data return it
+ 	// update response time accordingly
  	if (data) {
  		console.log('Cached.');
+ 		RESPONSE_TIME = Date.now() - start;
+ 		console.log('Complete: ' + RESPONSE_TIME + 'ms');
  		res.end(data);
  	} else {
- 		console.log('No cache.');
+ 		console.log('Requesting...');
+ 		// fetch data from url
+ 		// simultaneously cache the data
+ 		// and pipe to the response
+ 		// update response time accordingly
  		request(url)
- 			.pipe(cache.set(url))
+ 			.pipe(cache.set(url, function(){
+ 				RESPONSE_TIME = Date.now() - start;
+ 				console.log('Complete: ' + RESPONSE_TIME + 'ms');
+ 			}))
  			.pipe(res);
  	}
 });
