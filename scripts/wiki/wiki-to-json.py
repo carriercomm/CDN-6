135a42712af33c2369c4a306aa1582fb900167b6
Andrew Barba <abarba.77@gmail.com>
2014-12-04 10:08:07 -0500

    fix parse bugs

--- scripts/wiki/wiki-to-json.py
+++ scripts/wiki/wiki-to-json.py
@@ -1,12 +1,15 @@
+import sys
+
 """
 Parses a wiki dump and creates a JSON 
 key-value map of paths to frequency
 """
+
 print '{'
 for line in open(sys.argv[-1]):
 	parts = line.split(" ")
 	if parts[0] == "en":
-		count = int(l[-1])
+		count = int(parts[-1])
 		if count >= (10 * 1000 * 1000):
 			print '"%s": %d,' % (parts[1], count)
 print '}'
